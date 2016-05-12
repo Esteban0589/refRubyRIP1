3d8c5498775c970c947fa3abec80d1373d1e135d
esteban0589 <eesprimo26@gmail.com>
2016-05-12 05:17:00 +0000

    araña, nomralizacion y tokenizacion listas

--- arana.rb
+++ arana.rb
@@ -2,30 +2,32 @@ require 'chilkat'
 require 'rubygems'
 require 'nokogiri'   
 require 'open-uri'
+require 'active_support/inflector'
+require 'map'
 
 spider = Chilkat::CkSpider.new()
 
-#  The spider object crawls a single web site at a time.  As you'll see
-#  in later examples, you can collect outbound links and use them to
-#  crawl the web.  For now, we'll simply spider 10 pages of chilkatsoft.com
+
 spider.Initialize("www.metrolyrics.com")
 
-#  Add the 1st URL:
+#Se agrega primera pagina a analizar por la araña
 spider.AddUnspidered("http://www.metrolyrics.com/")
 
-# spider.AddMustMatchPattern("*-lyrics-*")
-# spider.AddAvoidPattern("*-lyrics.html")
-# spider.AddAvoidPattern("*-discussions-*")
-# spider.AddAvoidPattern("*news-story*")
-# spider.AddAvoidPattern("*printlyric*")
-
-#  Begin crawling the site by calling CrawlNext repeatedly.
+#Se abre documento donde se guardara el numero de documento, URL y letra de 
+#cancion para ser normalizada, parseada y tokenizada.
 output = File.open( "outputfile.txt","w" )
-#Buscara en 1000paginas
+
+
+#Por motivos de poder computacional se analiza un conjunto acotado de paginas web 
+#aunque se pretende que eventualmente podria ser escalable al valor deseado
+#
+
+mapa = Map.new()
 
 i=0
 while (i<3)
-
+    
+    #Se analiza la siguiente URL en la lista de paginas
     success = spider.CrawlNext()
     if (success == true)
         #este if verificara que la url tenga el patron de las paginas con letra de canciones
@@ -41,29 +43,74 @@ while (i<3)
             )
             #  Show the URL of the page just spidered.
             i=i+1
+        
+            # Se salva numero de documento, URL y letra de cancion encontrada
             output << i
             output <<  "\n"
-            
             output << spider.lastUrl() + "\n";
-
-            autor = Nokogiri::HTML(spider.lastHtml()).css("h2").map {|h2| h2.content}
-            output << autor[0].sub!(" Lyrics", "")
-            
-            titulo = Nokogiri::HTML(spider.lastHtml()).css("header h1").map {|header| header.content}
-            output << titulo[0].sub!(" Lyrics", "") ;
-            
-            # lyric = Nokogiri::HTML(spider.lastHtml()).css("div#lyrics-body-text").map {|div| div.content}
-            # output << lyric[0] + "\n";
             lyric = Nokogiri::HTML(spider.lastHtml()).css("div#lyrics-body-text p.verse").map {|div| div.content}
-            # print (lyric.join("\n"))
-            # print "\n"
-            # print "\n"
-           # lyric.sub!("\\n", "\n")
+            # print 
             
-            output << (lyric.join("\n"))
-
-            output <<  "\n"
-            output <<  "\n"
+            
+            #Todas las mayusculas a minusculas
+            downcasiado = (String(lyric)).downcase
+            
+            #Como viene del casteado arreglamos el hecho de que "\n" viene como 2 caracteres en vez de uno
+            arregleEspacios = downcasiado.gsub('\n',"\n")
+            
+            #Quitamos underscores del todo
+            sinUnderscore = arregleEspacios.gsub("_","")
+            
+            #Cambia espacios por '_' para que no se cambien cuando usemos el \W
+            espaciosPorUnderscore1 = sinUnderscore.gsub(" ","_")
+            espaciosPorUnderscore = espaciosPorUnderscore1.gsub("\n","_")
+            #aqui dejamos solo letras numeros y _(espacios)
+            stringSinSimbolos = espaciosPorUnderscore.gsub(/\W/,'')
+            #devolv
+            # soloFaltaDeSplitear= stringSinSimbolos.gsub("_"," ") #Commentado porque al momento del split se le dice que divida por _
+            
+            tokenizado = stringSinSimbolos.split("_")
+            mapa.map{|i| tokenizado}
+            
+            
+            output << tokenizado
+            output << "\n\n"
+            print mapa
+            
+            
+            
+            
+            
+            
+            
+            #  print (((((String(lyric)).downcase).gsub('\n',"\n")).gsub!(/\W | ' '/,'')).split(" "))
+            
+            # print lyric[0].downcase
+            # output << lyric[0].downcase
+            #  output << (lyric.join("\n") + "\n\n")
+            
+          
+            
+            
+            # Obtenemos pagina
+            # Escribimos numero de documento
+            # Escribimos url a como viene
+            # Obtenemos letra de cancion(conjunto de caracteres)(lyrics)
+            
+            # split(lyrics( " " && "\n"))->Array          Normalize todo lyrics
+            # Recorrer array analizando mediate regex     Haga split
+            
+            
+            # Tokenizamos (haciendo un split por espacios)()
+            # Normalizamos
+            
+            # Guardarlo en output
+            
+            
+            # Escribimos 
+            
+            
+            
             
             
             
@@ -83,4 +130,6 @@ while (i<3)
     #  Sleep 1 second before spidering the next URL.
     spider.SleepMs(1)
 end
+
+    # print ((('asd ASD! ASasdD'.downcase).split(" ")).gsub!(/\W/,''))
 output.close
